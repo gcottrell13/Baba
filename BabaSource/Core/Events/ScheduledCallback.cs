84b51d602cc7abba0395b4cd0e76daae2066909d
Gage <inquisitorpinkie@gmail.com>
2021-06-13 16:59:52 -0700

    added scheduled callback

---
+++ BabaSource/Core/Events/ScheduledCallback.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Events
+{
+    public class ScheduledCallback
+    {
+        public ScheduledCallback(double durationSeconds)
+        {
+            DurationSeconds = durationSeconds;
+        }
+
+        public Action? Callback;
+        public Action<double>? PerFrameCallback;
+        public double DurationSeconds;
+    }
+}
---
+++ BabaSource/Core/Utils/CallbackRunner.cs
@@ -0,0 +1,63 @@
+﻿using Core.Events;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core.Utils
+{
+    public class CallbackRunner : GameObject
+    {
+        private List<CallbackRecord> callbacks;
+        private readonly Action<Action<ScheduledCallback>> unsubFunction;
+
+        public CallbackRunner(Action<Action<ScheduledCallback>> subscribeFunction, Action<Action<ScheduledCallback>> unsubFunction)
+        {
+            callbacks = new List<CallbackRecord>();
+            subscribeFunction(OnEvent);
+            this.unsubFunction = unsubFunction;
+        }
+
+        protected override void OnDestroy()
+        {
+            unsubFunction(OnEvent);
+            base.OnDestroy();
+        }
+
+        private void OnEvent(ScheduledCallback ev)
+        {
+            if (ev.DurationSeconds <= 0f) return;
+
+            callbacks.Add(new CallbackRecord
+            {
+                ScheduledCallback = ev,
+                StartTime = DateTime.Now,
+                TimeSpan = TimeSpan.FromSeconds(ev.DurationSeconds),
+            });
+        }
+
+        protected override void OnUpdate(GameTime gameTime)
+        {
+            foreach (var cb in callbacks.ToList())
+            {
+                var elapsed = DateTime.Now - cb.StartTime;
+
+                cb.ScheduledCallback.PerFrameCallback?.Invoke(elapsed.TotalSeconds);
+
+                if (elapsed > cb.TimeSpan)
+                {
+                    callbacks.Remove(cb);
+                    cb.ScheduledCallback.Callback?.Invoke();
+                }
+            }
+        }
+
+        struct CallbackRecord
+        {
+            public ScheduledCallback ScheduledCallback;
+            public DateTime StartTime;
+            public TimeSpan TimeSpan;
+        }
+    }
+}

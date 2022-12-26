4fcbee1ccad1d72ed9e6a13b4274c9ee1587230c
Gage Cottrell <inquisitorpinkie@gmail.com>
2022-12-26 15:40:38 -0700

    add Editor project

---
+++ BabaSource/Editor/EditorGame.cs
@@ -0,0 +1,32 @@
+﻿using Core.Bootstrap;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Editor
+{
+    public class EditorGame : GameSetup
+    {
+        public EditorGame() : base(new EditorGameEntryPoint())
+        {
+            MAX_WIDTH = 1080;
+            MAX_HEIGHT = 720;
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+
+            // TODO: Add your update logic here
+        }
+
+        private class EditorGameEntryPoint : GameEntryPoint
+        {
+            public override void Initialize()
+            {
+            }
+        }
+    }
+}

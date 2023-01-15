ad25949e5631ed54c96365f4fed7d358415df17b
Gage Cottrell <inquisitorpinkie@gmail.com>
2023-01-15 14:07:00 -0700

    GetWordChains + tests

---
+++ BabaSource/Core/Engine/ParseSentences.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Engine;
+
+public class ParseSentences
+{
+    public interface INameable
+    {
+        string Name { get; }
+    }
+
+    private static IEnumerable<(int index, T item)> enumerate<T>(IEnumerable<T> collection)
+    {
+        foreach (var (index, item) in collection.Select((t, i) => (i, t)))
+        {
+            yield return (index, item);
+        }
+    }
+
+    public static List<List<T>> GetWordChains<T>(List<List<T?>> grid, HashSet<string?> words) where T : INameable
+    {
+        List<(int x, int y, string dir)> starts = new();
+        foreach (var (x, col) in enumerate(grid))
+        {
+            foreach (var (y, word) in enumerate(col))
+            {
+                if (word == null || words.Contains(word.Name) == false) continue;
+
+                if (x < grid.Count - 2 && words.Contains(grid[x + 1][y]?.Name))
+                    starts.Add((x, y, "right"));
+
+                if (y < grid[x].Count - 2 && words.Contains(grid[x][y + 1]?.Name))
+                    starts.Add((x, y, "down"));
+            }
+        }
+
+        HashSet<(int, int, string)> consumed = new();
+        List<List<T>> chains = new();
+
+        foreach (var m0 in starts)
+        {
+            var (x, y, dir) = m0;
+            var chain = new List<T>();
+
+            var m = (x, y, dir);
+
+            while (x < grid.Count && y < grid[x].Count && grid[x][y] is T t && words.Contains(t.Name))
+            {
+                if (consumed.Contains(m))
+                    goto Done;
+
+                consumed.Add(m);
+
+                chain.Add(t);
+
+                if (dir == "down")
+                    y += 1;
+                else if (dir == "right")
+                    x += 1;
+
+                m = (x, y, dir);
+            }
+
+            chains.Add(chain);
+
+            Done: { }
+        }
+
+        return chains;
+    }
+}
---
+++ BabaSource/Tests/Engine/ParseSentencesTests.cs
@@ -0,0 +1,111 @@
+﻿using Core.Engine;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Core.Engine.ParseSentences;
+
+namespace Tests.Engine
+{
+    [TestFixture]
+    internal class ParseSentencesTests
+    {
+
+        public class Item : INameable
+        {
+            public string Name { get; set; }
+            public static implicit operator Item(string s) { return new() { Name = s }; }
+            public override string ToString() => Name;
+            public override bool Equals(object? obj)
+            {
+                if (obj is string s) return s == Name;
+                if (obj is Item i) return i.Name == Name;
+                return base.Equals(obj);
+            }
+
+            public override int GetHashCode() => Name.GetHashCode();
+        }
+
+        [Test]
+        [TestCaseSource(nameof(GetChainsTestCases))]
+        public void GetChains_Test(List<List<Item?>> input, List<List<Item>> expected)
+        {
+            var chains = GetWordChains(input, new HashSet<string?> { "a", "b", "c" });
+            Assert.AreEqual(expected, chains);
+        }
+
+        static IEnumerable<TestCaseData> GetChainsTestCases => _getChainsTestCases.Select(
+            x => new TestCaseData(x.input, x.expected)
+            .SetName(x.name)
+        );
+
+        static IEnumerable<(string name, List<List<Item?>> input, List<List<Item>> expected)> _getChainsTestCases { get
+            {
+                yield return (
+                    "aaa",
+                    new() {
+                        new() { null, null, null, "a" },
+                        new() { null, null, null, "a" },
+                        new() { null, null, null, "a" },
+                    },
+                    new() {
+                        new() { "a", "a", "a" },
+                    });
+                yield return (
+                    "separate",
+                    new() {
+                        new() { "b", null, null, "a" },
+                        new() { "a", null, null, "a" },
+                        new() { "c", null, null, "a" },
+                    },
+                    new() {
+                        new() { "b", "a", "c" },
+                        new() { "a", "a", "a" },
+                    });
+                yield return (
+                    "cross",
+                    new() {
+                        new() { null, "b", null },
+                        new() { "a", "b", "a" },
+                        new() { null, "a", null },
+                    },
+                    new() {
+                        new() {"b", "b", "a"},
+                        new() {"a", "b", "a"},
+                    });
+                yield return (
+                    "cbacbacba",
+                    new() {
+                        new() { "c", "b", "a" },
+                        new() { "c", "b", "a" },
+                        new() { "c", "b", "a" },
+                        new() { "c", "b", "a" },
+                    },
+                    new() {
+                        new() { "c", "c", "c", "c" },
+                        new() { "c", "b", "a" },
+                        new() { "b", "b", "b", "b" },
+                        new() { "a", "a", "a", "a" },
+                        new() { "c", "b", "a" },
+                        new() { "c", "b", "a" },
+                        new() { "c", "b", "a" },
+                    });
+            } }
+
+        [Test]
+        public void GetWordChains_ReturnsSameReference()
+        {
+            var input = new List<List<Item?>>()
+            {
+                new() { "a", "b", "c", "b", "a" },
+            };
+
+            var chains = GetWordChains(input, new HashSet<string?> { "a", "b", "c" });
+
+            Assert.That(chains[0].Count == 5);
+            Assert.That(chains[0].Zip(input[0]).All(t => object.ReferenceEquals(t.First, t.Second)));
+        }
+    }
+}

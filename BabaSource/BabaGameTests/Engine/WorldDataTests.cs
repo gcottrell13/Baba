db05be044caac82da8894cc630f21c5f52219260
Gage Cottrell <inquisitorpinkie@gmail.com>
2023-01-18 16:09:53 -0700

    change world data test to only care about round trip (de)serialize

--- BabaSource/BabaGameTests/Engine/WorldDataTests.cs
+++ BabaSource/BabaGameTests/Engine/WorldDataTests.cs
@@ -13,33 +13,29 @@ namespace BabaGameTests.Engine
     {
         [Test]
         [TestCaseSource(nameof(_serializeTestCases))]
-        public void WorldData_Serialize(WorldObject[] input, string expected)
+        public void WorldData_Serialize(WorldObject[] input)
         {
             var data = new WorldData(input);
 
             var serialized = data.Serialize();
-            Assert.AreEqual(expected, serialized);
-
             var deserialized = WorldData.Deserialize(serialized).WorldObjects;
             Assert.AreEqual(input, deserialized);
         }
 
-        static IEnumerable<TestCaseData> _serializeTestCases => _serializeCaseGenerator.Select(x => new TestCaseData(x.input, x.expected).SetName(x.name));
+        static IEnumerable<TestCaseData> _serializeTestCases => _serializeCaseGenerator.Select(x => new TestCaseData(x.input).SetName(x.name));
 
-        static IEnumerable<(string name, WorldObject[] input, string expected)> _serializeCaseGenerator { get
+        static IEnumerable<(string name, WorldObject[] input)> _serializeCaseGenerator { get
         {
                 yield return (
                     "empty",
-                    Array.Empty<WorldObject>(),
-                    ""
+                    Array.Empty<WorldObject>()
                 );
                 yield return (
                     "one",
                     new[]
                     {
                         new WorldObject() { Occupied= true, Color=WorldObjectColor.Blue, Kind=WorldObjectKind.Text, ObjectId=1, Name="amongi" },
-                    },
-                    "\0\0\0Ȁ\0Ā"
+                    }
                 );
                 yield return (
                     "three",
@@ -48,8 +44,7 @@ namespace BabaGameTests.Engine
                         new WorldObject() { Occupied= true, Color=WorldObjectColor.Blue, Kind=WorldObjectKind.Text, ObjectId=1, Name="amongi" },
                         new WorldObject() { Occupied= true, Color=WorldObjectColor.Red, Kind=WorldObjectKind.Text, ObjectId=5, x=10, y=15, Name="badbad" },
                         new WorldObject() { Occupied= true, Color=WorldObjectColor.Faded | WorldObjectColor.Pink, Kind=WorldObjectKind.Object, ObjectId=400, Facing=Core.Utils.Direction.Left, Name="text_rubble" },
-                    },
-                    "\0\0\0Ȁ\0Ā਀ༀ\0Ā\0Ԁ\0\0Ѐ䤀Ā送"
+                    }
                 );
             } }
 

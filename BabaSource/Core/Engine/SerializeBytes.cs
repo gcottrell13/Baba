306efba3287755d48695049b76f45817d5ce6719
Gage Cottrell <inquisitorpinkie@gmail.com>
2023-01-21 14:30:22 -0700

    (de)serialize bytes

---
+++ BabaSource/Core/Engine/SerializeBytes.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Engine;
+
+
+public class SerializeBytes
+{
+    private static readonly Dictionary<Type, Func<object?, byte[]>> serializers = new()
+    {
+        { typeof(int),  getSerializerMethod<int>() },
+        { typeof(short),  getSerializerMethod<short>() },
+        { typeof(long),  getSerializerMethod<long>() },
+        { typeof(bool),  getSerializerMethod<bool>() },
+        { typeof(string),  getSerializerMethod<string>() },
+    };
+    private static readonly Dictionary<Type, Func<Enumerator<byte>, object>> deserializers = new()
+    {
+        { typeof(int),  (Enumerator<byte> x) => BitConverter.ToInt32(take(x, 4)) },
+        { typeof(short),  (Enumerator<byte> x) => BitConverter.ToInt16(take(x, 2)) },
+        { typeof(long),  (Enumerator<byte> x) => BitConverter.ToInt64(take(x, 8)) },
+        { typeof(bool),  (Enumerator<byte> x) => BitConverter.ToBoolean(take(x, 1)) },
+    };
+
+    private static Func<object?, byte[]> getSerializerMethod<T>()
+    {
+        if (typeof(T) == typeof(string))
+        {
+            return (object? x) => serializeString(x as string);
+        }
+        var method = typeof(BitConverter).GetMethods().First(x => x.Name == "GetBytes" && x.GetParameters().First().ParameterType == typeof(T));
+        return (object? x) => (byte[])method.Invoke(null, new[] { x })!;
+    }
+
+    private static byte[] serializeString(string? str) => str == null ? BitConverter.GetBytes((int)0) : BitConverter.GetBytes(str.Length).Concat(Encoding.UTF8.GetBytes(str)).ToArray();
+
+    private static string? deserializeString(Enumerator<byte> input)
+    {
+        var len = BitConverter.ToInt32(take(input, 4));
+        if (len == 0) return null;
+        var bytes = take(input, len);
+        return Encoding.UTF8.GetString(bytes);
+    }
+
+    private static byte[] take(Enumerator<byte> enumerator, int count)
+    {
+        var bytes = new byte[count];
+        if (count == 0) 
+            return bytes;
+
+        var c = 0;
+        while (enumerator.MoveNext())
+        {
+            bytes[c++] = enumerator.Current;
+            if (c >= count) break;
+        }
+
+        return bytes;
+    }
+
+    public static string SerializeObjects<T>(IEnumerable<T> objects)
+    {
+        var fields = typeof(T).GetFields();
+        var props = typeof(T).GetProperties().Where(x => x.CanWrite && x.CanRead).ToArray();
+
+        var bytes = new List<byte>();
+
+        foreach (var obj in objects)
+        {
+            foreach (var field in fields)
+            {
+                if(serializers.TryGetValue(field.FieldType, out var serializer))
+                    bytes.AddRange(serializer(field.GetValue(obj)));
+            }
+            foreach (var prop in props)
+            {
+                if (serializers.TryGetValue(prop.PropertyType, out var serializer))
+                    bytes.AddRange(serializer(prop.GetValue(obj)));
+            }
+        }
+
+        var str = Convert.ToBase64String(bytes.ToArray());
+        var output = "";
+        for (var i = 0; i < str.Length; i += 64)
+        {
+            if (str.Length - i < 64) output += str[i..];
+            else output += str[i..(i + 64)] + "\n";
+        }
+
+        return output;
+    }
+
+    public static List<T> DeserializeObjects<T>(string? str) where T : new()
+    {
+        if (str == null) return new();
+
+        var bytes = Convert.FromBase64String(str.Replace("\n", "").Replace("\r", "")).ToList();
+
+        var fields = typeof(T).GetFields();
+        var props = typeof(T).GetProperties().Where(x => x.CanWrite && x.CanRead).ToArray();
+
+        var enumerator = new Enumerator<byte>(bytes);
+
+        var arr = new List<T>();
+
+        while (enumerator.HasNext())
+        {
+            var item = (object)new T();
+            foreach (var field in fields)
+            {
+                object? value;
+                if (deserializers.TryGetValue(field.FieldType, out var deserializer))
+                {
+                    value = deserializer(enumerator);
+                }
+                else if (field.FieldType == typeof(string))
+                {
+                    value = deserializeString(enumerator);
+                }
+                else
+                    continue;
+                field.SetValue(item, value);
+            }
+            foreach (var prop in props)
+            {
+                object? value;
+                if (deserializers.TryGetValue(prop.PropertyType, out var deserializer))
+                {
+                    value = deserializer(enumerator);
+                }
+                else if (prop.PropertyType == typeof(string))
+                {
+                    value = deserializeString(enumerator);
+                }
+                else
+                    continue;
+                prop.SetValue(item, value);
+            }
+            arr.Add((T)item);
+        }
+
+        return arr;
+    }
+
+    [DebuggerDisplay("Enumerator at {index} = {Current}")]
+    private class Enumerator<T>
+    {
+        private readonly List<T> items;
+        public int index = -1;
+
+        public T Current => items[index];
+
+        public Enumerator(IEnumerable<T> items)
+        {
+            this.items = items.ToList();
+        }
+
+        public bool MoveNext()
+        {
+            return ++index < items.Count;
+        }
+
+        public bool HasNext() => index < items.Count - 1;
+    }
+}
---
+++ BabaSource/Tests/Engine/SerializeBytesTests.cs
@@ -0,0 +1,51 @@
+﻿using Core.Engine;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Engine
+{
+    [TestFixture]
+    public class SerializeBytesTests
+    {
+
+        public class TestClass
+        {
+            public int IntProp { get; set; }
+            public int IntField;
+            public short ShortProp { get; set; }
+            public short ShortField;
+
+            public string? StringProp { get; set; }
+            public string? StringField;
+
+            public override bool Equals(object? obj)
+            {
+                if (obj is TestClass c)
+                    return c.IntField== IntField && c.StringField==StringField && c.IntProp == IntProp && c.ShortProp == ShortProp && c.ShortField == ShortField && c.StringProp == StringProp;
+                return base.Equals(obj);    
+            }
+
+            public override string ToString() => $"{IntProp} {IntField} {ShortProp} {ShortField} {StringProp} {StringField}";
+        }
+
+        [Test]
+        public void ShouldSerializeItems()
+        {
+            var input = new List<TestClass>()
+            {
+                new TestClass { IntProp = 1, IntField=2, ShortField=3, ShortProp=4, StringField="hi", StringProp="there" },
+                new TestClass { IntProp = 10, IntField=2, ShortField=3, ShortProp=4, StringField="33333", StringProp="there2" },
+                new TestClass { IntProp = 10, IntField=2, ShortField=3, ShortProp=4, StringField=null, StringProp="there2" },
+                new TestClass { IntProp = 10, IntField=2, ShortField=3, ShortProp=4, StringField=null, StringProp=null },
+                new TestClass { IntProp = 10, IntField=2, ShortField=3, ShortProp=4, StringField="AAAA===/ds/fsdf", StringProp="there2" },
+            };
+            var serialized = SerializeBytes.SerializeObjects(input);
+            var deserialized = SerializeBytes.DeserializeObjects<TestClass>(serialized);
+            Assert.AreEqual(input, deserialized);
+        }
+    }
+}
